8e573a95917e457650838b6372507b32be236704
Khagesh Sharma <khageshhiet@gmail.com>
2016-06-21 21:41:29 +0530

    [#121848539] Added commands for sovrin

--- sovrin/cli/cli.py
+++ sovrin/cli/cli.py
@@ -1,3 +1,4 @@
+import ast
 from typing import Dict
 
 import time
@@ -6,6 +7,7 @@ import asyncio
 
 from plenum.cli.cli import Cli as PlenumCli
 from prompt_toolkit.contrib.completers import WordCompleter
+from prompt_toolkit.layout.lexers import SimpleLexer
 from pygments.token import Token
 
 from plenum.client.signer import Signer, SimpleSigner
@@ -57,14 +59,50 @@ class SovrinCli(PlenumCli):
             "(\s* (?P<client_command>{}) \s+ (?P<node_or_cli>clients?)   \s+ (?P<client_name>[a-zA-Z0-9]+) \s*) |".format(self.relist(self.cliCmds)),
             "(\s* (?P<client>client) \s+ (?P<client_name>[a-zA-Z0-9]+) \s+ (?P<cli_action>send) \s+ (?P<msg>\{\s*.*\})  \s*)  |",
             "(\s* (?P<client>client) \s+ (?P<client_name>[a-zA-Z0-9]+) \s+ (?P<cli_action>show) \s+ (?P<req_id>[0-9]+)  \s*)  |",
-            "(\s* (?P<add_key>add\s+key) \s+ (?P<verkey>[a-fA-F0-9]+) \s+ (?P<for_client>for\s+client) \s+ (?P<identifier>[a-zA-Z0-9]+) \s*)",
+            "(\s* (?P<add_key>add\s+key) \s+ (?P<verkey>[a-fA-F0-9]+) \s+ (?P<for_client>for\s+client) \s+ (?P<identifier>[a-zA-Z0-9]+) \s*) |",
+            "(\s* (?P<send_nym>send\s+NYM) \s+ (?P<dest>dest=)\s*(?P<dest_id>[a-fA-F0-9]+) \s*) |",
+            "(\s* (?P<send_get_nym>send\s+GET_NYM) \s+ (?P<dest>dest=)\s*(?P<dest_id>[a-fA-F0-9]+) \s*) |",
+            "(\s* (?P<send_attrib>send\s+ATTRIB) \s+ dest=\s*(?P<dest_id>[a-fA-F0-9]+) \s+ raw=(?P<raw>\{\s*.*\}) \s*) |",
+            "(\s* (?P<send_cred_def>send\s+CRED_DEF) \s+ name=\"\s*(?P<name>[a-zA-Z0-9\s]+)\" \s+ version=\"(?P<version>[0-9\.]+)\" " \
+                "\s+type=(?P<type>[a-zA-Z0-9]+) \s+ ip=(?P<ip>[0-9\.]+) \s+ port=(?P<port>[0-9]+) \s+ keys=(?P<keys>\{\s*.*\}) \s*) |",
+            "(\s* (?P<send_cred>send\s+to) \s+ (?P<dest>[a-fA-F0-9]+) \s+ (?P<saveas>saveas)? \s+? (?P<cred_name>[a-zA-Z0-9\-_]+)? \s+? REQ_CRED \s+ " \
+                " name=(?P<name>[a-zA-Z0-9\-]+) \s+ version=(?P<version>[0-9\.]+) \s+ attrs=(?P<attrs>[a-zA-Z0-9,]+) \s*) |",
+            "(\s* (?P<list_cred>list\s+CRED) \s*) |",
+            "(\s* (?P<send_proof>send\s+proof) \s+ of \s+ (?P<attr_name>[a-zA-Z0-9\-_]+) \s+ from \s+ (?P<cred_name>[a-zA-Z0-9\-_]+)? \s+ to \s+ " \
+            " (?P<dest>[a-fA-F0-9]+) \s*) |",
+            # TODO:KS Change base class to allow for extension, as of now we don't have any clean API for it
+            "(\s* (?P<new_keypair>new_keypair) \s* (?P<alias>[a-zA-Z0-9]+)? \s*) |",
+            "(\s* (?P<list_ids>list) \s+ (?P<ids>ids) \s*) |",
+            "(\s* (?P<become>become) \s+ (?P<id>[a-zA-Z0-9]+) \s*) |",
+            "(\s* (?P<use_keypair>use_keypair) \s+ (?P<keypair>[a-fA-F0-9]+) \s*)"
         ]
         super().initializeGrammar()
+
+    def initializeGrammarLexer(self):
+        sovrinLexers = {
+            'send_nym': SimpleLexer(Token.Keyword),
+            'send_get_nym': SimpleLexer(Token.Keyword),
+            'send_attrib': SimpleLexer(Token.Keyword),
+            'send_cred_def': SimpleLexer(Token.Keyword),
+            'send_cred': SimpleLexer(Token.Keyword),
+            'list_cred': SimpleLexer(Token.Keyword),
+            'send_proof': SimpleLexer(Token.Keyword)
+        }
+        # Add more lexers to base class lexers
+        self.lexers = {**self.lexers, **sovrinLexers}
+        super().initializeGrammarLexer()
 
     def initializeGrammarCompleter(self):
         self.nymWC = WordCompleter([])
         self.completers["nym"] = self.nymWC
         self.completers["role"] = WordCompleter(["user", "sponsor"])
+        self.completers["send_nym"] = WordCompleter(["send", "NYM"])
+        self.completers["send_get_nym"] = WordCompleter(["send", "GET_NYM"])
+        self.completers["send_attrib"] = WordCompleter(["send", "ATTRIB"])
+        self.completers["send_cred_def"] = WordCompleter(["send", "CRED_DEF"])
+        self.completers["send_cred"] = WordCompleter(["send", "to"])
+        self.completers["list_cred"] = WordCompleter(["list", "CRED"])
+        self.completers["send_proof"] = WordCompleter(["send", "proof"])
         super().initializeGrammarCompleter()
 
     def loadGenesisTxns(self):
@@ -148,8 +186,72 @@ class SovrinCli(PlenumCli):
                                                 signer)
                     return True
 
+    def _sendNymAction(self, matchedVars):
+        if matchedVars.get('send_nym') == 'send NYM':
+            destId = matchedVars.get('dest_id')
+            # TODO:LH Add code to send dest
+            print("dest id is {}".format(destId))
+            return True
+
+    def _sendGetNymAction(self, matchedVars):
+        if matchedVars.get('send_get_nym') == 'send GET_NYM':
+            destId = matchedVars.get('dest_id')
+            # TODO:LH Add code to send GET_NYM
+            print("dest id is {}".format(destId))
+            return True
+
+    def _sendAttribAction(self, matchedVars):
+        if matchedVars.get('send_attrib') == 'send ATTRIB':
+            destId = matchedVars.get('dest_id')
+            raw = ast.literal_eval(matchedVars.get('raw'))
+            # TODO:LH Add code to send GET_NYM
+            print("dest id is {}".format(destId))
+            print("raw message is {}".format(raw))
+            return True
+
+    def _sendCredDefAction(self, matchedVars):
+        if matchedVars.get('send_cred_def') == 'send CRED_DEF':
+            name = matchedVars.get('name')
+            version = matchedVars.get('version')
+            type = matchedVars.get('type')
+            ip = matchedVars.get('ip')
+            port = matchedVars.get('port')
+            keys = ast.literal_eval(matchedVars.get('keys'))
+            # TODO:LH Add code to send GET_NYM
+            print("passed values are {}, {}, {}, {}, {}, {}".format(name, version, type, ip, port, keys))
+            return True
+
+    def _sendCredAction(self, matchedVars):
+        print(matchedVars)
+        if matchedVars.get('send_cred') == 'send to':
+            dest = matchedVars.get('dest')
+            credName = matchedVars.get('cred_name')
+            name = matchedVars.get('name')
+            version = matchedVars.get('version')
+            attrs = matchedVars.get('attrs')
+            print("passed values are {}, {}, {}, {}, {}".format(dest, credName, name, version, attrs))
+            return True
+
+    def _listCredAction(self, matchedVars):
+        if matchedVars.get('list_cred') == 'list CRED':
+            # TODO:LH Add method to list creds
+            return True
+
+    def _sendProofAction(self, matchedVars):
+        if matchedVars.get('send_proof') == 'send proof':
+            attrName = matchedVars.get('attr_name')
+            credName = matchedVars.get('cred_name')
+            dest = matchedVars.get('dest')
+            print("{}, {}, {}".format(attrName, credName, dest))
+            return True
+
     def getActionList(self):
         actions = super().getActionList()
+        # Add more actions to base class for sovrin CLI
+        actions.extend([self._sendNymAction, self._sendGetNymAction,
+                        self._sendAttribAction, self._sendCredDefAction,
+                        self._sendCredAction, self._listCredAction,
+                        self._sendProofAction])
         return actions
 
     @staticmethod
