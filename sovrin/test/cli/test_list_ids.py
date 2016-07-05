cf4670b655a7aa569baa20e4c7fd8b437e448471
Rajesh Kalaria <rajesh.kalaria@gmail.com>
2016-07-05 15:38:03 +0530

    added cli package into test

---
+++ sovrin/test/cli/test_list_ids.py
@@ -0,0 +1,11 @@
+
+
+# def testListIds(cli):
+#     cli.enterCmd("new_keypair test")
+#     assert len(cli.defaultClient.signers) == 2
+#     pubKeyMsg = cli.lastPrintArgs['msg']
+#     assert pubKeyMsg.startswith('Public key is')
+#     pubKey = pubKeyMsg.split(" ")[-1]
+#     cli.enterCmd("list ids")
+#     assert cli.lastPrintArgs['msg'] == "test"
+
