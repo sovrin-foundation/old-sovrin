462a84410b16afc24ee5bc545af7d132eec0d6b3
Lovesh <lovesh@evernym.us>
2016-07-11 00:44:30 +0530

    removing redundant code

--- sovrin/test/cli/test_add_genesis_transaction.py
+++ sovrin/test/cli/test_add_genesis_transaction.py
@@ -14,24 +14,15 @@ def testAddGenesisTransactions(cli):
     typ = NYM
     checkCmdValid(cli, "add genesis transaction {} dest={} role={}"
                   .format(typ, nym, role))
-    txn = {
-        TXN_TYPE: typ,
-        TARGET_NYM: nym,
-        TXN_ID: sha256(randomString(6).encode()).hexdigest(),
-        ROLE: role,
-    }
-    typeCorrect = False
     nymCorrect = False
     roleCorrect = False
 
     for txn in cli.genesisTransactions:
         if not nymCorrect and txn.get(TARGET_NYM) == nym:
             nymCorrect = True
-            if txn.get(TXN_TYPE) == typ:
-                typeCorrect = True
             if txn.get(ROLE) == role:
                 roleCorrect = True
 
-    assert nymCorrect and typeCorrect and roleCorrect
+    assert nymCorrect and roleCorrect
     assert "Genesis transaction added" in cli.lastCmdOutput
 
