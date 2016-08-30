eebaa476e83a6699059ec556a3b82ad213f83294
Lovesh <lovesh@evernym.us>
2016-08-30 11:40:19 +0530

    referencing object using module names as reloading a test module is not taking affect

--- sovrin/test/anon_creds/test_anon_cred_wallet.py
+++ sovrin/test/anon_creds/test_anon_cred_wallet.py
@@ -1,20 +1,17 @@
-from sovrin.anon_creds.cred_def import CredDef
-from sovrin.anon_creds.issuer import AttribDef, AttribType
+import sovrin.anon_creds.cred_def as cred_def
+import sovrin.anon_creds.issuer as issuer
 from sovrin.client.wallet import Wallet
 from sovrin.persistence.wallet_storage_file import WalletStorageFile
 
 
-GVT = AttribDef('gvt',
-                [AttribType('name', encode=True),
-                  AttribType('age', encode=False),
-                  AttribType('sex', encode=True)])
-
-X = CredDef
-
 def testCredDefSecretKey(tdir):
-    P_PRIME1, Q_PRIME1 = CredDef.getStaticPPrime("prime1"), CredDef.getStaticQPrime("prime1")
+    GVT = issuer.AttribDef('gvt',
+                           [issuer.AttribType('name', encode=True),
+                            issuer.AttribType('age', encode=False),
+                            issuer.AttribType('sex', encode=True)])
+    P_PRIME1, Q_PRIME1 = cred_def.CredDef.getStaticPPrime("prime1"), cred_def.CredDef.getStaticQPrime("prime1")
     primes = dict(p_prime=P_PRIME1, q_prime=Q_PRIME1)
-    gvtCredDef = CredDef(GVT.attribNames(), **primes)
+    gvtCredDef = cred_def.CredDef(GVT.attribNames(), **primes)
     serializedSk = gvtCredDef.serializedSK
     walletStorage = WalletStorageFile(tdir)
     wallet = Wallet("testWallet", walletStorage)
