f689c68e66eababe0d55870d97c9aa855f13ae52
Neelkanth Sachdeva <neelkanthsachdeva@gmail.com>
2016-08-30 14:23:01 +0530

    [#128831433] script created to check the publick key and verification key - refactoring after review - some other modifications revisited

--- scripts/get_keys.py
+++ scripts/get_keys.py
@@ -1,10 +1,6 @@
 #This script takes an argument which is either Node name or client name
 # and returns the public key & verification key
 
-print("\n=======================================================================================================================")
-print("\nThis is a script to get the public key and verification key &")
-print("Takes either node name or client name to get the keys\n")
-
 import json
 import os
 import sys
@@ -22,32 +18,29 @@ CURRENT_LOGGED_IN_USER = "sovrin"
 
 path = '/home/'+ CURRENT_LOGGED_IN_USER +'/.sovrin/'  + NODE_OR_CLIENT_NAME +'/role/local/role.json'
 
-if(os.path.exists(path)):
+if not os.path.exists(path):
+    raise Exception("Sorry, please check the client or node name you've entered")
+with open(path, "r") as f:
+    keyString = f.read().strip()
+try:
+    d = json.loads(keyString)
+except json.decoder.JSONDecodeError:
+    raise Exception("non json content exception message here")
+if 'prihex' not in d:
+    raise ValueError("key not defined in given data")
+if 'sighex' not in d:
+    raise ValueError("key not defined in given data")
 
-    with open(path, "r") as f:
-        keyString = f.read().strip()
-    try:
-        d = json.loads(keyString)
-    except json.decoder.JSONDecodeError:
-        raise Exception("non json content exception message here")
 
-    if 'prihex' not in d:
-        raise ValueError("key not defined in given data")
-    if 'sighex' not in d:
-        raise ValueError("key not defined in given data")
+prihex = d['prihex']
+sighex = d['sighex']
+privateer = Privateer(prihex)
+pubkey = privateer.pubhex.decode()
+signer = Signer(sighex)
+verifkey = signer.verhex.decode()
 
-    prihex = d['prihex']
-    sighex = d['sighex']
-    privateer = Privateer(prihex)
-    pubkey = privateer.pubhex.decode()
-    signer = Signer(sighex)
-    verifkey = signer.verhex.decode()
+print("\nPublic key is : " + pubkey)
+print("\nVerification key is : " + verifkey)
 
-    print("\nPublic key is : " + pubkey)
-    print("\nVerification key is : " + verifkey)
 
-else:
-    print("Sorry, please check the client or node name you've entered")
 
-print("\n\nThank you !\n")
-print("\n=======================================================================================================================")
