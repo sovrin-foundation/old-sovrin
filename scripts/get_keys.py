3e864b2ff30ac20a706ece3f1d4165a662bbc996
Neelkanth Sachdeva <neelkanthsachdeva@gmail.com>
2016-08-30 12:56:05 +0530

    [#128831433] script created to check the publick key and verification key - refactoring after review

--- scripts/get_keys.py
+++ scripts/get_keys.py
@@ -12,31 +12,45 @@ import sys
 from raet.nacling import Signer, Privateer
 
 EMPTY_STRING = ''
+
+if(len(sys.argv) > 2):
+    raise Exception('provide only one parameter which specifies node or client name')
+if(len(sys.argv) < 2):
+    raise Exception('provide the parameter which specifies node or client name')
+
 NODE_OR_CLIENT_NAME = sys.argv.pop(1)
-CURRENT_LOGGED_IN_USER = os.getlogin()
+CURRENT_LOGGED_IN_USER = "sovrin"
 
 path = '/home/'+ CURRENT_LOGGED_IN_USER +'/.sovrin/'  + NODE_OR_CLIENT_NAME +'/role/local/role.json'
 
 if(os.path.exists(path)):
 
     files=glob.glob(path)
-    for file in files:
-        f = open(file, 'r')
-        keyString = EMPTY_STRING.join(f.readlines())
-        d = json.loads(keyString)
-        prihex = d['prihex']
-        sighex = d['sighex']
-        f.close()
+    f = open(files.pop(0), 'r')
+    keyString = EMPTY_STRING.join(f.readlines())
 
+    try:
+     d = json.loads(keyString)
+    except Exception:
+     print("invalid file content from file that contains keys")
+
+    if 'prihex' not in d:
+        raise ValueError("No target in given data")
+    if 'sighex' not in d:
+        raise ValueError("No target in given data")
+
+    prihex = d['prihex']
+    sighex = d['sighex']
+    f.close()
     privateer = Privateer(prihex)
     pubkey = privateer.pubhex.decode()
-
     signer = Signer(sighex)
     verifkey = signer.verhex.decode()
 
     print("\nPublic key is : " + pubkey)
     print("\nVerification key is : " + verifkey)
-
+    # except Exception:
+    #  print("invalid file content from file that contains keys")
 else:
     print("Sorry, please check the client or node name you've entered")
 
