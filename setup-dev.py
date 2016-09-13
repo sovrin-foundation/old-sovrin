3f8fe89dfe0995ad13e1b5ff6a72039fb07105de
Lovesh <lovesh@evernym.us>
2016-09-13 13:54:42 +0530

    adding script to setup

--- setup-dev.py
+++ setup-dev.py
@@ -6,7 +6,6 @@ from pip.req import parse_requirements
 from shutil import copyfile
 import data
 
-
 v = sys.version_info
 if sys.version_info < (3, 5):
     msg = "FAIL: Requires Python 3.5 or later, " \
@@ -53,10 +52,11 @@ setup(
     author_email='dev@evernym.us',
     license=__license__,
     keywords='Sovrin identity plenum',
-    packages=find_packages(exclude=['test', 'test.*', 'docs', 'docs*']) + ['data', ],
+    packages=find_packages(exclude=['test', 'test.*', 'docs', 'docs*']) + [
+        'data', ],
     package_data={
-        '':       ['*.txt',  '*.md', '*.rst', '*.json', '*.conf', '*.html',
-                   '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL']},
+        '': ['*.txt', '*.md', '*.rst', '*.json', '*.conf', '*.html',
+             '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL']},
     include_package_data=True,
     data_files=[(
         (BASE_DIR, ['data/pool_transactions', ])
@@ -66,14 +66,17 @@ setup(
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     scripts=['scripts/sovrin', 'scripts/init_sovrin_raet_keep',
-             'scripts/start_sovrin_node', 'scripts/generate_sovrin_pool_transactions']
+             'scripts/start_sovrin_node',
+             'scripts/generate_sovrin_pool_transactions', 'scripts/get_keys']
 )
 
 if not os.path.exists(CONFIG_FILE):
     with open(CONFIG_FILE, 'w') as f:
-        msg = "# Here you can create config entries according to your needs.\n " \
+        msg = "# Here you can create config entries according to your " \
+              "needs.\n " \
               "# For help, refer config.py in the sovrin package.\n " \
-              "# Any entry you add here would override that from config example\n"
+              "# Any entry you add here would override that from config " \
+              "example\n"
         f.write(msg)
 
 DATA_DIR = os.path.dirname(data.__file__)
