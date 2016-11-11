f35c5f4b1e24aba5e9c9c2445bd6b04da65d554c
Rajesh Kalaria <rajesh.kalaria@gmail.com>
2016-11-11 15:53:06 +0530

    modified agent to fetch cred def and issuer key from ledger if available

--- sovrin/test/agent/test_walleted_agent.py
+++ sovrin/test/agent/test_walleted_agent.py
@@ -43,17 +43,48 @@ class TestWalletedAgent(WalletedAgent):
 
         claimDef = self.wallet.getClaimDef(key=claimDefKey)
         chk = partial(self.wallet.isClaimDefComplete, claimDefKey)
-        if chk():
+
+        def getIssuerKeyIfClaimDefPresentInWallet(claimDef):
             # # Assuming if credential definition is present on ledger the
             # # issuer key would be
             self.wallet.getIssuerPublicKey(key=(origin, claimDef.seqNo))
-            clbk()
-        else:
-            claimDef = self.wallet.createClaimDef(name=name, version=version,
-                                                  attrNames=attrNames, typ=typ)
-            req, = self.wallet.preparePending()
+            isk = self.wallet._generateIssuerSecretKey(claimDef, csk=credDefSecretKey)
+            self.wallet.addIssuerSecretKey(isk)
+            if clbk:
+                clbk()
+
+        def checkInWalletElseCreate(reply, err):
+            claimDef = self.wallet.getClaimDef(key=claimDefKey)
+
+            logger.debug("Claim def chk result: {}".format(chk()))
+            if chk():
+                getIssuerKeyIfClaimDefPresentInWallet(claimDef)
+            else:
+                claimDef = self.wallet.createClaimDef(name=name, version=version,
+                                                      attrNames=attrNames, typ=typ)
+                req, = self.wallet.preparePending()
+                self.client.submitReqs(req)
+                self.loop.call_later(.2, ensureReqCompleted, self.loop,
+                                     req.reqId, self.client,
+                                     postClaimDefWritten, (claimDef,), None,
+                                     chk)
+
+        def getIssuerKeyFromLedger(reply, err):
+            claimDef = self.wallet.getClaimDef(key=claimDefKey)
+            req = self.wallet.requestIssuerKey((origin, claimDef.seqNo),
+                                               self.wallet.defaultId)
             self.client.submitReqs(req)
-            self.loop.call_later(.2, ensureReqCompleted, self.loop,
-                                 req.reqId, self.client,
-                                 postClaimDefWritten, (claimDef,), None,
-                                 chk)
+            self.loop.call_later(.2, ensureReqCompleted, self.loop, req.reqId,
+                                 self.client, checkInWalletElseCreate,
+                                 None, None, None)
+
+        if not chk():
+            req = self.wallet.requestClaimDef(claimDefKey=claimDefKey,
+                                              sender=self.wallet.defaultId)
+            self.client.submitReqs(req)
+            self.loop.call_later(.2, ensureReqCompleted, self.loop, req.reqId,
+                                 self.client, getIssuerKeyFromLedger,
+                                 None, None, None)
+
+        else:
+            getIssuerKeyIfClaimDefPresentInWallet(claimDef)
