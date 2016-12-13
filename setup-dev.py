b3e3b5ba9ed8e259f0d5549d30d6e5d4d4b7b596
andrey.goncharov <andrey.goncharov@dsr-company.com>
2016-12-13 15:43:00 +0300

    Dependencies hot fix

--- setup-dev.py
+++ setup-dev.py
@@ -67,7 +67,7 @@ setup(
     install_requires=['base58', 'pyorient', 'plenum-dev', 'ledger-dev',
                       'semver', 'anoncreds-dev'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest==3.0.2'],
+    tests_require=['pytest'],
     scripts=['scripts/sovrin', 'scripts/init_sovrin_raet_keep',
              'scripts/start_sovrin_node',
              'scripts/generate_sovrin_pool_transactions', 'scripts/get_keys']
