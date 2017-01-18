fd51fd587e485eb690354a0fddcd757c42de286f
Rajesh Kalaria <rajesh.kalaria@gmail.com>
2017-01-18 09:26:00 +0530

    fixed failing test cases

--- sovrin/test/conftest.py
+++ sovrin/test/conftest.py
@@ -15,6 +15,8 @@ from plenum.common.looper import Looper
 from plenum.common.signer_simple import SimpleSigner
 from plenum.common.txn import VERKEY
 from plenum.test.plugin.helper import getPluginPath
+from plenum.test.conftest import patchPluginManager
+
 from sovrin.client.wallet.wallet import Wallet
 from sovrin.common.txn import STEWARD, NYM, SPONSOR
 from sovrin.common.txn import TXN_TYPE, TARGET_NYM, TXN_ID, ROLE, \
