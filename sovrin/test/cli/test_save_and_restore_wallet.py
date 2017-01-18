aa4b07f91ee1d36d4d93ee1b9949032d6ca1b222
Rajesh Kalaria <rajesh.kalaria@gmail.com>
2017-01-18 09:26:12 +0530

    added more tests around cli wallet persistence

--- sovrin/test/cli/test_save_and_restore_wallet.py
+++ sovrin/test/cli/test_save_and_restore_wallet.py
@@ -1,5 +1,6 @@
 import os
 from os.path import basename
+from time import sleep
 
 import pytest
 from plenum.cli.cli import Exit, Cli
@@ -96,8 +97,16 @@ def _connectTo(envName, do, cli):
     prompt_is("{}@{}".format(cli.name, envName))
 
 
-def connectTo(envName, do, cli, activeWalletPresents, identifiers):
+def connectTo(envName, do, cli, activeWalletPresents, identifiers,
+              firstTimeConnect=False):
+    currActiveWallet = cli._activeWallet
     _connectTo(envName, do, cli)
+    if currActiveWallet is None and firstTimeConnect:
+        do(None, expect=[
+            "New keyring Default created",
+            'Active keyring set to "Default"']
+        )
+
     if activeWalletPresents:
         assert cli._activeWallet is not None
         assert len(cli._activeWallet.identifiers) == identifiers
@@ -116,13 +125,31 @@ def switchEnv(newEnvName, do, cli, checkIfWalletRestored=False,
         checkWalletRestored(restoredWalletKeyName, cli, restoredIdentifiers)
 
 
-def testSaveAndRestoreWallet(do, be, cliForMultiNodePools):
+def exit(do):
+    with pytest.raises(Exit):
+        do('exit', expect='Goodbye.')
+
+
+def restartCli(CliBuilder, be, do, expectedRestoredWalletName,
+               expectedIdentifiers):
+    cli = yield from CliBuilder("alice")
+    be(cli)
+    do(None, expect=[
+        'Running Sovrin',
+        'Saved keyring "{}" restored'.format(expectedRestoredWalletName),
+        'Active keyring set to "{}"'.format(expectedRestoredWalletName)
+    ], within=5)
+    assert cli._activeWallet is not None
+    assert len(cli._activeWallet.identifiers) == expectedIdentifiers
+
+
+def testSaveAndRestoreWallet(do, be, cliForMultiNodePools, CliBuilder):
     be(cliForMultiNodePools)
     # No wallet should have been restored
     assert cliForMultiNodePools._activeWallet is None
 
     connectTo("pool1", do, cliForMultiNodePools,
-              activeWalletPresents=True, identifiers=0)
+              activeWalletPresents=True, identifiers=0, firstTimeConnect=True)
     createNewKey(do, cliForMultiNodePools, keyringName="Default")
 
     switchEnv("pool2", do, cliForMultiNodePools, checkIfWalletRestored=False)
@@ -132,7 +159,7 @@ def testSaveAndRestoreWallet(do, be, cliForMultiNodePools):
     createNewKey(do, cliForMultiNodePools, keyringName="mykr0")
     useKeyring("Default", do)
     createNewKey(do, cliForMultiNodePools, keyringName="Default")
-
+    sleep(10)
     switchEnv("pool1", do, cliForMultiNodePools, checkIfWalletRestored=True,
               restoredWalletKeyName="Default", restoredIdentifiers=1)
     createNewKeyring("mykr1", do)
@@ -151,4 +178,7 @@ def testSaveAndRestoreWallet(do, be, cliForMultiNodePools):
               restoredWalletKeyName="mykr1", restoredIdentifiers=1)
     baseName = basename(filePath)
     useKeyring(filePath, do, expectedName="mykr0",
-               expectedMsgs=["Saved keyring {} restored".format(baseName)])
+               expectedMsgs=['Saved keyring "{}" restored'.format(baseName)])
+    exit(do)
+
+    restartCli(CliBuilder, be, do, "Default", 1)
